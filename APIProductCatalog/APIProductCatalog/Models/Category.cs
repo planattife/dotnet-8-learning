e109506201f96da3facf9eb3fd3d7d41155be875
Pedro Ferrareso <pedrolf30@gmail.com>
2024-12-23 10:50:16 -0300

    creating and running migrations

--- APIProductCatalog/APIProductCatalog/Models/Category.cs
+++ APIProductCatalog/APIProductCatalog/Models/Category.cs
@@ -1,7 +1,26 @@
-﻿namespace APIProductCatalog.Models;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APIProductCatalog.Models;
+
+[Table("Categories")]
 public class Category
 {
+    public Category()
+    {
+        Products = new Collection<Product>();
+    }
+
+    [Key]
     public int CategoryId { get; set; }
+    
+    [Required]
+    [StringLength(80)]
     public string? Name { get; set; }
+    
+    [Required]
+    [StringLength(300)]
     public string? ImageUrl { get; set; }
+    public ICollection<Product>? Products { get; set; }
 }
--- APIProductCatalog/APIProductCatalog/Models/Product.cs
+++ APIProductCatalog/APIProductCatalog/Models/Product.cs
@@ -1,11 +1,30 @@
-﻿namespace APIProductCatalog.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APIProductCatalog.Models;
+
+[Table("Products")]
 public class Product
 {
+    [Key]
     public int ProductId { get; set; }
+    [Required]
+    [StringLength(80)]
     public string? Name { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? Description { get; set; }
+
+    [Required]
+    [Column(TypeName = "decimal(10,2)")]
     public decimal Price { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImageUrl { get; set; }
     public int Quantity { get; set; }
     public DateTime CreatedAt { get; set; }
+    public int CategoryId { get; set; }
+    public Category? Category { get; set; }
 }
